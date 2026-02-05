b84c9b31dd23b90f7bc4d7605e93f363efb7563b
macedo <gabrimacedo@posteo.com>
2026-02-05 20:52:11 -0300

    add initial display attempt, add instructions (8-C), add some tests

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
+use rand::{Rng, rngs::ThreadRng};
+
 fn main() {
-    let mut processor = Chip8::new();
+    let mut chip = Chip8::new();
+    chip.load_instructions(&[0x6042, 0x7005, 0x4047, 0x3047]);
 
-    // fetch instruction from memory 0 and forth
-    let code = processor.fetch();
+    loop {
+        // fetch instruction from memory 0 and forth
+        let op = chip.fetch();
+        if op == 0 {
+            break;
+        }
+        // decode instruction
+        chip.decode(op);
+        // execute instruction
+        dbg!(chip.v(0));
+    }
 
-    // decode instruction
-    processor.decode(code);
+    let four: &[u8] = &[0x90, 0x90, 0xf0, 0x10, 0x10];
+    let two: &[u8] = &[0xf0, 0x10, 0xf0, 0x80, 0xf0];
+    let zero: &[u8] = &[0xf0, 0x90, 0x90, 0x90, 0xf0];
 
-    // execute instruction
+    for (i, byte) in four.iter().enumerate() {
+        // coords are (3,5)
+        // that is 3rd line, 63 - 5 bit
+        let x = (*byte as u64) << 52;
+        chip.display[3 + i] ^= x;
+    }
+    for (i, byte) in two.iter().enumerate() {
+        // coords are (3,5)
+        // that is 3rd line, 63 - 5 bit
+        let x = (*byte as u64) << 47;
+        chip.display[3 + i] ^= x;
+    }
+    for (i, byte) in zero.iter().enumerate() {
+        // coords are (3,5)
+        // that is 3rd line, 63 - 5 bit
+        let x = (*byte as u64) << 42;
+        chip.display[3 + i] ^= x;
+    }
+
+    chip.print_display();
 }
 
 struct Chip8 {
@@ -15,6 +47,8 @@ struct Chip8 {
     registers: [u8; 16],
     pc: u16,
     i: u16,
+    rng: ThreadRng,
+    display: [u64; 32],
 }
 
 impl Chip8 {
@@ -23,15 +57,49 @@ impl Chip8 {
             memory: [0; 4096],
             registers: [0; 16],
             i: 0,
-            pc: 0,
+            pc: 0x200,
+            rng: rand::thread_rng(),
+            display: [0; 32],
+        }
+    }
+
+    fn print_display(&self) {
+        for line in self.display {
+            let mut shift = 63;
+            println!();
+
+            while shift > 0 {
+                let bit = (line >> shift) & 0x1;
+
+                if bit == 1 {
+                    print!("x");
+                } else {
+                    print!(" ");
+                }
+
+                shift -= 1;
+            }
+        }
+    }
+
+    fn load_instructions(&mut self, instructions: &[u16]) {
+        let mut i = 0x200;
+
+        for opcode in instructions {
+            let high = (opcode >> 8) as u8;
+            let low = (opcode & 0xff) as u8;
+
+            self.memory[i] = high;
+            self.memory[i + 1] = low;
+            i += 2;
         }
     }
 
     fn fetch(&self) -> u16 {
         let high = self.memory[self.pc as usize] as u16;
-        let low = self.memory[(self.pc + 1) as usize];
+        let low = self.memory[(self.pc + 1) as usize] as u16;
 
-        (high << 2) + (low as u16)
+        (high << 8) + low
     }
 
     fn v(&self, reg: u16) -> u8 {
@@ -54,9 +122,6 @@ impl Chip8 {
         self.pc += 2;
 
         match op_code {
-            0 => todo!(),
-            1 => todo!(),
-            2 => todo!(),
             3 => {
                 if vx == kk {
                     self.pc += 2
@@ -74,38 +139,49 @@ impl Chip8 {
             }
             6 => self.set_v(x, kk),
             7 => self.set_v(x, vx + kk),
-            8 => {
-                match nibble {
-                    0 => self.set_v(x, vy),
-                    1 => self.set_v(x, vx | vy),
-                    2 => self.set_v(x, vx & vy),
-                    3 => self.set_v(x, vx ^ vy),
-                    4 => {
-                        let sum = vx as u16 + vy as u16;
-                        self.set_v(x, sum as u8);
-                        self.set_v(0xf, (sum > 255) as u8);
-                    }
-                    5 => {
-                        self.set_v(0xf, (vx >= vy) as u8);
-                        self.set_v(x, vx.wrapping_sub(vy));
-                    }
-                    // Set Vx = Vx SHR 1
-                    6 => {}
-                    // Set Vx = Vy - Vx, set VF = NOT borrow
-                    7 => {}
-                    // Set Vx = Vx SHL 1
-                    0xE => {}
-                    _ => todo!(),
+            8 => match nibble {
+                0 => self.set_v(x, vy),
+                1 => self.set_v(x, vx | vy),
+                2 => self.set_v(x, vx & vy),
+                3 => self.set_v(x, vx ^ vy),
+                4 => {
+                    let sum = vx as u16 + vy as u16;
+                    self.set_v(x, sum as u8);
+                    self.set_v(0xf, (sum > 255) as u8);
+                }
+                5 => {
+                    self.set_v(0xf, (vx >= vy) as u8);
+                    self.set_v(x, vx.wrapping_sub(vy));
+                }
+                6 => {
+                    self.set_v(0xf, (vx & 0x1 == 1) as u8);
+                    self.set_v(x, vx >> 1);
+                }
+                7 => {
+                    self.set_v(0xf, (vy >= vx) as u8);
+                    self.set_v(x, vy.wrapping_sub(vx));
+                }
+                0xE => {
+                    self.set_v(0xf, ((vx >> 7) & 0x1 == 1) as u8);
+                    self.set_v(x, vx << 1);
+                }
+                _ => todo!(),
+            },
+            9 => {
+                if vx != vy {
+                    self.pc += 2
                 }
             }
-            9 => todo!(),
-            10 => todo!(),
-            11 => todo!(),
-            12 => todo!(),
-            13 => todo!(),
-            14 => todo!(),
-            15 => todo!(),
-            _ => todo!(),
+            0xA => self.i = adrr,
+            0xB => self.pc = adrr + (self.v(0)) as u16,
+            0xC => {
+                let r = self.rng.r#gen::<u8>();
+                self.set_v(x, r & kk);
+            }
+            0xD => todo!(),
+            0xE => todo!(),
+            0xF => todo!(),
+            _ => println!("dont look here yet, code is {op_code}"),
         }
         println!("Counter is {}", self.pc)
     }
@@ -113,7 +189,35 @@ impl Chip8 {
 
 #[cfg(test)]
 mod test {
+    use crate::Chip8;
 
     #[test]
-    fn fetch_correct_code() {}
+    fn fetch_correct_instructions() {
+        let mut chip = Chip8::new();
+        let instructions = [0x6042, 0x7005, 0x4047, 0x3047];
+        chip.load_instructions(&instructions);
+
+        for intruct in instructions {
+            let op = chip.fetch();
+            chip.decode(op);
+
+            assert_eq!(op, intruct);
+        }
+    }
+
+    #[test]
+    // add 200 to 10, then add 110 to 210 to test overflow
+    fn add_numbers() {
+        let mut chip = Chip8::new();
+        let instructions = [0x600A, 0x61C8, 0x8014, 0x616E, 0x8014];
+        chip.load_instructions(&instructions);
+
+        for _ in instructions {
+            let op = chip.fetch();
+            chip.decode(op);
+        }
+
+        let result = (chip.v(0xf) as u16) << 8 | (chip.v(0) as u16);
+        assert_eq!(result, 320);
+    }
 }
