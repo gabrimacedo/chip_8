8317936a3a9192a4d3fe05a976ed970c5f66e6ab
macedo <gabrimacedo@posteo.com>
2026-02-08 00:03:18 -0300

    fix addr bytes in decode, add the displa instruction and some test instructions

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@ use rand::{Rng, rngs::ThreadRng};
 
 fn main() {
     let mut chip = Chip8::new();
-    chip.load_instructions(&[0x6042, 0x7005, 0x4047, 0x3047]);
 
-    loop {
-        // fetch instruction from memory 0 and forth
+    let instructions = [
+        0x611E, // load coord (x) 30 into v1
+        0x620B, // load coord (y) 11 into v2
+        0xA032, // Set I to adrress of sprite 'A'
+        0xD125, // display 5 bytes wide letter stored in I
+        0x620D, // load coord (y) 13 into v2
+        0xA041, // Set I to adrress of sprite 'D'
+        0xD125, // display 5 bytes wide letter stored in I
+    ];
+
+    chip.load_instructions(&instructions);
+
+    for _ in instructions {
         let op = chip.fetch();
-        if op == 0 {
-            break;
-        }
-        // decode instruction
         chip.decode(op);
-        // execute instruction
-        dbg!(&chip.v(0));
     }
 
-    let four: &[u8] = &[0x90, 0x90, 0xf0, 0x10, 0x10];
-    let two: &[u8] = &[0xf0, 0x10, 0xf0, 0x80, 0xf0];
-    let zero: &[u8] = &[0xf0, 0x90, 0x90, 0x90, 0xf0];
+    dbg!(chip.v(0xf));
 }
 
 struct Chip8 {
@@ -32,8 +34,36 @@ struct Chip8 {
 
 impl Chip8 {
     fn new() -> Self {
+        let digit_sprites: [[u8; 5]; 15] = [
+            [0xf0, 0x90, 0x90, 0x90, 0xf0], // 0
+            [0x20, 0x60, 0x20, 0x20, 0x70], // 1
+            [0xf0, 0x10, 0xf0, 0x80, 0xf0], // 2
+            [0xf0, 0x10, 0xf0, 0x10, 0xf0], // 3
+            [0x90, 0x90, 0xf0, 0x10, 0x10], // 4
+            [0xf0, 0x80, 0xf0, 0x10, 0xf0], // 5
+            [0xf0, 0x80, 0xf0, 0x90, 0xf0], // 6
+            [0xf0, 0x10, 0x20, 0x40, 0x40], // 7
+            [0xf0, 0x90, 0xf0, 0x90, 0xf0], // 8
+            [0xf0, 0x90, 0xf0, 0x10, 0xf0], // 9
+            [0xf0, 0x90, 0xf0, 0x90, 0x90], // A
+            [0xe0, 0x90, 0xe0, 0x90, 0xe0], // B
+            [0xf0, 0x80, 0x80, 0x80, 0xf0], // C
+            [0xe0, 0x90, 0x90, 0x90, 0xe0], // D
+            [0xf0, 0x80, 0xf0, 0x80, 0x80], // F
+        ];
+
+        let mut memory: [u8; 4096] = [0; 4096];
+
+        let mut i = 0;
+        for digit in digit_sprites {
+            for byte in digit {
+                memory[i] = byte;
+                i += 1;
+            }
+        }
+
         Chip8 {
-            memory: [0; 4096],
+            memory,
             registers: [0; 16],
             display: [0; 32],
             i: 0,
@@ -42,23 +72,29 @@ impl Chip8 {
         }
     }
 
-    fn load_sprite_data(display: &mut [u64; 32], (x, y): (u8, u8), sprite: &[u8]) {
-        // TODO: make it prettier danmit
+    fn draw(display: &mut [u64; 32], (x, y): (u8, u8), vf: &mut u8, sprite: &[u8]) {
+        // load to buffer
         for (i, byte) in sprite.iter().enumerate() {
-            let z = (*byte as u64) << (63 - 8 - x);
-            display[(y + i as u8) as usize] ^= z;
+            let sprite = (*byte as u64) << (63 - 8 - x);
+            display[(y + i as u8) as usize] ^= sprite;
+
+            //check for erased pixels
+            let current = display[(y + i as u8) as usize];
+            let erased = (current >> (63 - (y + 5))) as u8 == *byte;
+            if erased {
+                *vf = 1;
+            } else {
+                *vf = 0;
+            }
         }
-    }
 
-    fn clear_display(&self) {}
-
-    fn print_display(&self) {
-        for line in self.display {
+        // print
+        for line in display {
             let mut shift = 63;
             println!();
 
             while shift > 0 {
-                let bit = (line >> shift) & 0x1;
+                let bit = (*line >> shift) & 0x1;
 
                 if bit == 1 {
                     print!("x");
@@ -100,10 +136,10 @@ impl Chip8 {
     }
 
     fn decode(&mut self, code: u16) {
-        let op_code = code >> 12;
-        let adrr = code >> 4;
-        let nibble = code & 0xf;
+        let op_code = (code >> 12) as u8;
+        let adrr = code & 0x0fff;
         let kk = (code & 0xff) as u8;
+        let nibble = code & 0xf;
         let x = (code >> 8) & 0xf;
         let y = (code >> 4) & 0xf;
         let vx = self.v(x);
@@ -112,45 +148,45 @@ impl Chip8 {
         self.pc += 2;
 
         match op_code {
-            0 => todo!(),
-            1 => self.pc = adrr,
-            2 => todo!(),
-            3 => {
+            0x0 => todo!(),
+            0x1 => self.pc = adrr,
+            0x2 => todo!(),
+            0x3 => {
                 if vx == kk {
                     self.pc += 2
                 }
             }
-            4 => {
+            0x4 => {
                 if vx != kk {
                     self.pc += 2
                 }
             }
-            5 => {
+            0x5 => {
                 if vx == vy {
                     self.pc += 2
                 }
             }
-            6 => self.set_v(x, kk),
-            7 => self.set_v(x, vx + kk),
-            8 => match nibble {
-                0 => self.set_v(x, vy),
-                1 => self.set_v(x, vx | vy),
-                2 => self.set_v(x, vx & vy),
-                3 => self.set_v(x, vx ^ vy),
-                4 => {
+            0x6 => self.set_v(x, kk),
+            0x7 => self.set_v(x, vx + kk),
+            0x8 => match nibble {
+                0x0 => self.set_v(x, vy),
+                0x1 => self.set_v(x, vx | vy),
+                0x2 => self.set_v(x, vx & vy),
+                0x3 => self.set_v(x, vx ^ vy),
+                0x4 => {
                     let sum = vx as u16 + vy as u16;
                     self.set_v(x, sum as u8);
                     self.set_v(0xf, (sum > 255) as u8);
                 }
-                5 => {
+                0x5 => {
                     self.set_v(0xf, (vx >= vy) as u8);
                     self.set_v(x, vx.wrapping_sub(vy));
                 }
-                6 => {
+                0x6 => {
                     self.set_v(0xf, (vx & 0x1 == 1) as u8);
                     self.set_v(x, vx >> 1);
                 }
-                7 => {
+                0x7 => {
                     self.set_v(0xf, (vy >= vx) as u8);
                     self.set_v(x, vy.wrapping_sub(vx));
                 }
@@ -160,7 +196,7 @@ impl Chip8 {
                 }
                 _ => println!("Invalid instruction code: {op_code}"),
             },
-            9 => {
+            0x9 => {
                 if vx != vy {
                     self.pc += 2
                 }
@@ -173,11 +209,27 @@ impl Chip8 {
             }
             0xD => {
                 let sprite_data = &self.memory[self.i as usize..(self.i + nibble) as usize];
-                Chip8::load_sprite_data(&mut self.display, (vx, vy), sprite_data); // todo pass vf
-                self.print_display();
+                Self::draw(
+                    &mut self.display,
+                    (vx, vy),
+                    &mut self.registers[0xf],
+                    sprite_data,
+                );
             }
             0xE => todo!(),
-            0xF => todo!(),
+            0xF => match kk {
+                0x07 => todo!(),
+                0x0A => todo!(),
+                0x15 => todo!(),
+                0x18 => todo!(),
+                0x1E => self.i += vx as u16,
+                // sprite data (0 - F) starts at adrr 0, and are 5 bytes long
+                0x29 => self.i = self.memory[(vx * 5) as usize] as u16,
+                0x33 => todo!(),
+                0x55 => todo!(),
+                0x65 => todo!(),
+                _ => println!("Invalid instruction code: {op_code}"),
+            },
             _ => println!("Invalid instruction code: {op_code}"),
         }
         println!("Counter is {}", self.pc)
@@ -187,6 +239,27 @@ impl Chip8 {
 #[cfg(test)]
 mod test {
     use crate::Chip8;
+
+    #[test]
+    fn display_letters() {
+        let mut chip = Chip8::new();
+        let instructions = [
+            0x611E, // load coord (x) 30 into v1
+            0x620B, // load coord (y) 11 into v2
+            0xA032, // Set I to adrress of sprite 'A'
+            0xD125, // display 5 bytes wide letter stored in I
+            0x620D, // load coord (y) 13 into v2
+            0xA041, // Set I to adrress of sprite 'D'
+            0xD125, // display 5 bytes wide letter stored in I
+        ];
+
+        chip.load_instructions(&instructions);
+
+        for _ in instructions {
+            let op = chip.fetch();
+            chip.decode(op);
+        }
+    }
 
     #[test]
     fn fetch_correct_instructions() {
